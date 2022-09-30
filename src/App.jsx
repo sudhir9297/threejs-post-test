50c00103f59ae80a418a7655728a34294db11699
sudhir <sudhir9297@gmail.com>
2022-09-30 17:51:56 +0530

    dist included

--- src/App.jsx
+++ src/App.jsx
@@ -272,21 +272,16 @@ class App extends React.Component {
       },
 
       vertexShader: `
-      
           varying vec2 vUv;
-      
           void main() {
-      
             vUv = uv;
             gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      
           }
           `,
-
       fragmentShader: `
         uniform sampler2D tDiffuse;
         uniform vec2 resolution;
-  
+
         vec2 barrelDistortion(vec2 coord, float amt) {
           vec2 cc = coord - 0.5;
           float dist = dot(cc, cc);
@@ -312,7 +307,6 @@ class App extends React.Component {
           float hi = 1.0-lo;
           float w = linterp( remap( t, 1.0/6.0, 5.0/6.0 ) );
           ret = vec4(lo,1.0,hi, 1.) * vec4(1.0-w, w, 1.0-w, 1.);
-  
           return pow( ret, vec4(1.0/2.2) );
         }
   
